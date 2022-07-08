16fea80d814b21685c8b669608081436724fd5f9
alexandre <alexandrenikitin@outlook.com>
2022-07-08 00:05:53 -0300

    salvando

--- JavaScript/ProjetoAvaliativo/src/js/index.js
+++ JavaScript/ProjetoAvaliativo/src/js/index.js
@@ -21,10 +21,8 @@ const target = document.querySelectorAll ('[data-anime]');
 
 const animationClass = 'animate';
 
-const some = 'some'
-
 function animeScroll(){
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3.5) / 4);
     target.forEach(function(element){
         if((windowTop) > element.offsetTop){
             element.classList.add(animationClass)
